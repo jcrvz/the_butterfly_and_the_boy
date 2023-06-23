6e5f63e33ead79c2dc035bb6c37555486c6a47d9
Jorge Mario Cruz-Duarte <jorge.cruz@itesm.mx>
2023-06-23 15:50:09 -0600

    audio added

--- scripts/Gameover.gd
+++ scripts/Gameover.gd
@@ -4,7 +4,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Signals.kill_player.connect(game_over)
-
+	$VBoxContainer/RestartButton.grab_focus()
 
 func _on_button_pressed():
 	get_tree().reload_current_scene()
--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$VBoxContainer/StartButton.grab_focus()
+	$VBoxContainer/BosqueButton.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,9 +11,13 @@ func _process(delta):
 	pass
 
 
-func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/scene1.tscn")
-
-
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_bosque_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/bosque.tscn")
+
+
+func _on_ciudad_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/ciudad.tscn")
